4b83f957d45b2feca2fe6675f3602d0c16881197
Joshua Uzzell <josh.uzzell@gmail.com>
2024-04-20 22:41:00 -0400

    Remove css-framework and design-tokens page

--- src/pages/design-tokens/index.js
+++
@@ -1,9 +0,0 @@
-import "../../main.js";
-import "../../components/home-page-component.js"
-
-(function(){
-    setTimeout(() => {
-        document.querySelector( 'body' ).removeAttribute( 'data-loading-screen' ); 
-        document.querySelector( 'body' ).setAttribute( 'data-overlay', 'inactive' );
-    }, 750)
-})();
