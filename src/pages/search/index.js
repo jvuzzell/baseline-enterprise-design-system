0d50ae2d21d8d5fd5be49e610e94d5f2adbd5ec0
Joshua Uzzell <josh.uzzell@gmail.com>
2024-04-22 17:50:44 -0400

    Update convention for page components

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,3 +1,3 @@
-import "../../components/doc-site-components/search-page/DocSearchBar.js";
-import "../../components/doc-site-components/search-page/SearchResults.js";
+import "../../components/doc-site-components/_page-search/DocSearchBar.js";
+import "../../components/doc-site-components/_page-search/SearchResults.js";
 import "../index";
